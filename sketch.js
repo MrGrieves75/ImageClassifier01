974464bc39825e1c2dd4097fa8995e0d9b4d63af
MrGrieves75 <magnificent.7@free.fr>
2025-02-14 16:53:42 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 let imageClassifier;
-let imagesPath = ["Assets/cat00001.jpg", "Assets/cat00002.jpg", "Assets/cat00003.jpg",
-                  "Assets/cat00004.jpg","Assets/cat00005.jpg","Assets/cat00006.jpg",
-                "Assets/cat00007.jpg", "Assets/cat00008.jpg", "Assets/cat00009.jpg",
-                  "Assets/cat00010.jpg","Assets/cat00011.jpg","Assets/cat00012.jpg",
-                "Assets/dog00001.jpg", "Assets/dog00002.jpg", "Assets/dog00003.jpg",
-                  "Assets/dog00004.jpg","Assets/dog00005.jpg","Assets/dog00006.jpg",
-                "Assets/dog00007.jpg"]
+let imagesPath = ["./Assets/cat00001.jpg", "./Assets/cat00002.jpg", "./Assets/cat00003.jpg",
+                  "./Assets/cat00004.jpg","./Assets/cat00005.jpg","./Assets/cat00006.jpg",
+                "./Assets/cat00007.jpg", "./Assets/cat00008.jpg", "./Assets/cat00009.jpg",
+                  "./Assets/cat00010.jpg","./Assets/cat00011.jpg","./Assets/cat00012.jpg",
+                "./Assets/dog00001.jpg", "./Assets/dog00002.jpg", "./Assets/dog00003.jpg",
+                  "./Assets/dog00004.jpg","./Assets/dog00005.jpg","./Assets/dog00006.jpg",
+                "./Assets/dog00007.jpg"]
 
 function setup() {
   createCanvas(800, 600);
